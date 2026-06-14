de2669eaa705f7306aa319e13e92a9b6a96932a2
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2026-06-14 10:06:22 +0300

    update

--- src/KeyValueStore.Application/SimpleStore.cs
+++ src/KeyValueStore.Application/SimpleStore.cs
@@ -2,16 +2,64 @@
 
 namespace KeyValueStore.Application;
 
-public class SimpleStore : IKeyValueStore
+public sealed class SimpleStore : IKeyValueStore, IDisposable
 {
     private readonly Dictionary<string, byte[]> _store = [];
+    private readonly ReaderWriterLockSlim _lock = new();
+
+    private long _setCount;
+    private long _getCount;
+    private long _deleteCount;
 
     public byte[]? Get(string key)
-        => _store.TryGetValue(key, out var item) ? item : null;
+    {
+        _lock.EnterReadLock();
+        try
+        {
+            Interlocked.Increment(ref _getCount);
+            return _store.TryGetValue(key, out var item) ? item : null;
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
+    }
 
     public void Set(string key, byte[] value)
-        => _store[key] = value;
+    {
+        _lock.EnterWriteLock();
+        try
+        {
+            Interlocked.Increment(ref _setCount);
+            _store[key] = value;
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
 
     public void Delete(string key)
-        => _store.Remove(key);
+    {
+        _lock.EnterWriteLock();
+        try
+        {
+            Interlocked.Increment(ref _deleteCount);
+            _store.Remove(key);
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+
+    public (long setCount, long getCount, long deleteCount) GetStatistics()
+    {
+        return (_setCount, _getCount, _deleteCount);
+    }
+
+    public void Dispose()
+    {
+        _lock.Dispose();
+    }
 }
